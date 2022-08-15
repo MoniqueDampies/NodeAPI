5fdd0aabb396eb7b0833ecfbce2a129c5026e8d4
MoniqueDampies <moniquedampies1861@gmail.com>
2022-08-15 15:02:10 +0200

    new

--- index.js
+++ index.js
@@ -25,10 +25,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Headers", "*");
     res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
+
     next();
 });
 
